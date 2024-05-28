e1c4874606d66612176a04918b48c02da59dc989
Tilak <patidartilak100@gmail.com>
2024-05-28 16:22:12 -0400

    Add new tests for atmost two number string

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -10,5 +10,17 @@ RSpec.describe StringCalculator do
     context 'when number is empty' do
       it { is_expected.to eq 0 }
     end
+
+    context 'when number has single number' do
+      let(:numbers) { '1' }
+
+      it { is_expected.to eq 1 }
+    end
+
+    context 'when number has two numbers' do
+      let(:numbers) { '1,2' }
+
+      it { is_expected.to eq 3 }
+    end
   end
 end
