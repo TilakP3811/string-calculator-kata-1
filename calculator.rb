ea4fb9032b946cf770dc865557ce07d96dbb2004
Tilak <patidartilak100@gmail.com>
2024-05-28 17:07:27 -0400

    Fix failing tests for step 4 add new delimiter changer logic

--- calculator.rb
+++ calculator.rb
@@ -5,6 +5,21 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    numbers.tr('\n', ',').split(',').map(&:to_i).sum
+    delimiter, numbers = extract_delimiter(numbers)
+
+    numbers.tr('\n', delimiter).split(delimiter).map(&:to_i).sum
+  end
+
+  private
+
+  def extract_delimiter(numbers)
+    delimiter = ','
+
+    if numbers.start_with?('//')
+      delimiter_line, numbers = numbers.split('\n', 2)
+      delimiter = delimiter_line[-1]
+    end
+
+    [delimiter, numbers]
   end
 end
