4cfc9aa9e4a4334e1810d26b87b08e001ad057d3
Tilak <patidartilak100@gmail.com>
2024-05-28 16:37:47 -0400

    Add test for third step where next-line symbol is allowed

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -4,31 +4,37 @@ require 'rspec'
 require_relative '../calculator'
 
 RSpec.describe StringCalculator do
-  subject { StringCalculator.new.add(numbers) }
+  subject { StringCalculator.new.add(input) }
 
-  let(:numbers) { '' }
+  let(:input) { '' }
 
   describe '#add' do
-    context 'when number is empty' do
+    context 'when input is empty' do
       it { is_expected.to eq 0 }
     end
 
-    context 'when number has single number' do
-      let(:numbers) { '1' }
+    context 'when input has single number' do
+      let(:input) { '1' }
 
       it { is_expected.to eq 1 }
     end
 
-    context 'when number has two numbers' do
-      let(:numbers) { '1,2' }
+    context 'when input has two numbers' do
+      let(:input) { '1,2' }
 
       it { is_expected.to eq 3 }
     end
 
-    context 'when number has unknown amount of numbers' do
-      let(:numbers) { '1,2,3,4,5' }
+    context 'when input has unknown amount of numbers' do
+      let(:input) { '1,2,3,4,5' }
 
       it { is_expected.to eq 15 }
+    end
+
+    context 'when input has next-line(\n) symbol between numbers' do
+      let(:input) { '1\n2,3' }
+
+      it { is_expected.to eq 6 }
     end
   end
 end
